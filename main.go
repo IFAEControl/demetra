875f9df20b57bf6f0699ee8387c102fb925bba8c
David Roman <davidroman96@gmail.com>
2020-11-30 22:59:03 +0100

    create yocto.go

--- main.go
+++ main.go
@@ -1,142 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"github.com/pborman/getopt/v2"
 	"log"
 	"os"
 )
-
-func setupSingleLayer(b *Bash, uri, release string, clean bool, layers ...string) {
-	setupRepo(b, uri, "", release, clean)
-	for _, l := range layers {
-		b.Run("check_layer", l)
-	}
-}
-
-func setupLayers(b *Bash, layers []repo, release string, clear bool) {
-	// first setup default layers
-	default_layers := []repo{
-		{"git@gitlab.pic.es:ifaecontrol/meta-dev.git", []string{"meta-dev"}},
-		{"git@gitlab.pic.es:ifaecontrol/meta-ifae.git", []string{"meta-ifae"}},
-		{"git://git.yoctoproject.org/meta-xilinx", []string{"meta-xilinx/meta-xilinx-bsp"}},
-		{"git://git.openembedded.org/meta-openembedded",
-			[]string{
-				"meta-openembedded/meta-oe",
-				"meta-openembedded/meta-python",
-				"meta-openembedded/meta-networking",
-			},
-		},
-	}
-
-	for _, l := range default_layers {
-		setupSingleLayer(b, l.Uri, release, clear, l.Layers...)
-	}
-
-	// then setup extra layers
-	for _, l := range layers {
-		setupSingleLayer(b, l.Uri, release, clear, l.Layers...)
-	}
-}
-
-func setupBuildDir(b *Bash, sd string) {
-	build_dir := fmt.Sprint(sd, "/build")
-
-	if !Exists(build_dir) {
-		cmd := fmt.Sprint("cd ", sd, "; source ./oe-init-build-env > /dev/null")
-		b.Run("bash", "-c", cmd)
-	}
-
-	if !Exists(build_dir) {
-		log.Fatal("Error when creating poky build directory")
-	}
-}
-
-func rebuildLocalCfg(b *Bash, sd string) {
-	local_conf := fmt.Sprint(sd, "/build/conf/local.conf")
-	// ignore error, if config not exist will be created
-	os.Remove(local_conf)
-	setupBuildDir(b, sd)
-}
-
-func cloneRepo(b *Bash, repo, directory string) string {
-	if directory != "" {
-		if Exists(directory) {
-			return directory
-		}
-
-		b.Run("git", "clone", repo, directory)
-	} else {
-		directory = GetStem(repo)
-		cloneRepo(b, repo, directory)
-	}
-
-	return directory
-}
-
-func setupRepo(b *Bash, repo, directory, release string, clean bool) {
-	if directory == "" {
-		directory = GetStem(repo)
-	}
-
-	if !Exists(directory) {
-		directory = cloneRepo(b, repo, directory)
-	}
-
-	old_dir, _ := os.Getwd()
-	err := os.Chdir(directory)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if clean {
-		b.Run("git", "checkout", "--", ".")
-		b.Run("git", "clean", "-fd")
-	}
-
-	b.Run("git", "pull")
-	b.Run("checkout_repository", release)
-
-	err = os.Chdir(old_dir)
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-func setupYocto(b *Bash, cfg tomlConfig, external bool, password string, clean bool) {
-	b.Source("scripts/helper_functions.sh")
-
-	setupRepo(b, "git://git.yoctoproject.org/poky", cfg.SetupDir, cfg.Release, clean)
-	rebuildLocalCfg(b, cfg.SetupDir)
-
-	err := os.Chdir(cfg.SetupDir)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	b.Run("checkout_machine", cfg.Machine)
-	b.Run("set_password", password)
-
-	conf := NewLocalConf()
-	defer conf.Close()
-
-	if external {
-		conf.append("INHERIT += \"externalsrc\"")
-
-		for key, value := range cfg.Srcs {
-			k := "EXTERNALSRC_pn" + key
-			path := Expand(value.Path)
-			conf.set(k, path)
-
-			if value.Module {
-				k := "EXTERNALSRC_BUILD_pn-" + key
-				conf.set(k, path)
-			}
-		}
-	}
-
-	setupLayers(b, cfg.Repo, cfg.Release, clean)
-}
 
 func main() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
@@ -166,7 +34,8 @@ func main() {
 
 	b := NewBash()
 
-	setupYocto(b, cfg, *external, *password, !*no_clean)
+	yocto := Yocto{b, cfg, *external, *password, !*no_clean}
+	yocto.setupYocto()
 
 	// build
 	if *build {
---
+++ yocto.go
@@ -0,0 +1,146 @@
+package main
+
+import (
+	"log"
+	"os"
+	"fmt"
+)
+
+type Yocto struct {
+	b        *Bash
+	cfg      tomlConfig
+	external bool
+	password string
+	clean    bool
+}
+
+func (y Yocto) setupSingleLayer(uri, release string, layers ...string) {
+	y.setupRepo(uri, "", release)
+	for _, l := range layers {
+		y.b.Run("check_layer", l)
+	}
+}
+
+func (y Yocto) setupLayers(layers []repo, release string) {
+	// first setup default layers
+	default_layers := []repo{
+		{"git@gitlab.pic.es:ifaecontrol/meta-dev.git", []string{"meta-dev"}},
+		{"git@gitlab.pic.es:ifaecontrol/meta-ifae.git", []string{"meta-ifae"}},
+		{"git://git.yoctoproject.org/meta-xilinx", []string{"meta-xilinx/meta-xilinx-bsp"}},
+		{"git://git.openembedded.org/meta-openembedded",
+			[]string{
+				"meta-openembedded/meta-oe",
+				"meta-openembedded/meta-python",
+				"meta-openembedded/meta-networking",
+			},
+		},
+	}
+
+	for _, l := range default_layers {
+		y.setupSingleLayer(l.Uri, release, l.Layers...)
+	}
+
+	// then setup extra layers
+	for _, l := range layers {
+		y.setupSingleLayer(l.Uri, release, l.Layers...)
+	}
+}
+
+func (y Yocto) setupBuildDir(sd string) {
+	build_dir := fmt.Sprint(sd, "/build")
+
+	if !Exists(build_dir) {
+		cmd := fmt.Sprint("cd ", sd, "; source ./oe-init-build-env > /dev/null")
+		y.b.Run("bash", "-c", cmd)
+	}
+
+	if !Exists(build_dir) {
+		log.Fatal("Error when creating poky build directory")
+	}
+}
+
+func (y Yocto) rebuildLocalCfg(sd string) {
+	local_conf := fmt.Sprint(sd, "/build/conf/local.conf")
+	// ignore error, if config not exist will be created
+	os.Remove(local_conf)
+	y.setupBuildDir(sd)
+}
+
+func (y Yocto) cloneRepo(repo, directory string) string {
+	if directory != "" {
+		if Exists(directory) {
+			return directory
+		}
+
+		y.b.Run("git", "clone", repo, directory)
+	} else {
+		directory = GetStem(repo)
+		y.cloneRepo(repo, directory)
+	}
+
+	return directory
+}
+
+func (y Yocto) setupRepo(repo, directory, release string) {
+	if directory == "" {
+		directory = GetStem(repo)
+	}
+
+	if !Exists(directory) {
+		directory = y.cloneRepo(repo, directory)
+	}
+
+	old_dir, _ := os.Getwd()
+	err := os.Chdir(directory)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if y.clean {
+		y.b.Run("git", "checkout", "--", ".")
+		y.b.Run("git", "clean", "-fd")
+	}
+
+	y.b.Run("git", "pull")
+	y.b.Run("checkout_repository", release)
+
+	err = os.Chdir(old_dir)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func (y Yocto) setupYocto() {
+	y.b.Source("scripts/helper_functions.sh")
+
+	y.setupRepo("git://git.yoctoproject.org/poky", y.cfg.SetupDir, y.cfg.Release)
+	y.rebuildLocalCfg(y.cfg.SetupDir)
+
+	err := os.Chdir(y.cfg.SetupDir)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	y.b.Run("checkout_machine", y.cfg.Machine)
+	y.b.Run("set_password", y.password)
+
+	conf := NewLocalConf()
+	defer conf.Close()
+
+	if y.external {
+		conf.append("INHERIT += \"externalsrc\"")
+
+		for key, value := range y.cfg.Srcs {
+			k := "EXTERNALSRC_pn" + key
+			path := Expand(value.Path)
+			conf.set(k, path)
+
+			if value.Module {
+				k := "EXTERNALSRC_BUILD_pn-" + key
+				conf.set(k, path)
+			}
+		}
+	}
+
+	y.setupLayers(y.cfg.Repo, y.cfg.Release)
+}
