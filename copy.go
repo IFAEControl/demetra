17add8b5024f2a96d7c69c1629764b5632f21839
David Roman <droman@ifae.es>
2021-06-05 16:12:41 +0200

    implement commonCopy

--- copy.go
+++ copy.go
@@ -27,7 +27,7 @@ func CopyImage(b *Bash, dest_dir, src, device, machine, bitstream string, clean 
 		RemoveContents(dest_dir)
 	}
 
-	b.Run("../scripts/common-copy.sh", dest_dir, src, machine, bitstream)
+	commonCopy(dest_dir, src, machine, bitstream)
 
 	if device != "" {
 		b.Run("udisksctl unmount -b", device)
@@ -46,7 +46,7 @@ func CopyRemoteImage(b *Bash, src, machine, bitstream string, password, ssh_ip s
 		runtime.Goexit()
 	}
 
-	b.Run("../scripts/common-copy.sh", dest, src, machine, bitstream)
+	commonCopy(dest_dir, src, machine, bitstream)
 
 	ssh := NewSshSession(ssh_ip, password)
 	defer ssh.Close()
@@ -77,4 +77,53 @@ func CopyRemoteImage(b *Bash, src, machine, bitstream string, password, ssh_ip s
 
 	ssh.Run("killall gfaserverd gfaserver &> /dev/null")
 	ssh.Run("reboot")
+}
+
+func commonCopy(b *Bash, dest, src, machine, bitstream string) {
+	//Explained in https://github.com/Xilinx/meta-xilinx/blob/master/README.booting.md#loading-via-sd
+
+	switch machine {
+	case "mercury-zx5":
+		// Generate boot.bin for enclustra
+		Copy(src + "/u-boot.elf", "resources/binaries")
+		Copy(bitstream, "resources/binaries/fpga.bit")
+
+		old_dir, _ := os.Getwd()
+		err := os.Chdir("resources/binaries")
+		LogAndExit(err)
+
+		b.Run("mkbootimage boot.bif /tmp/boot.bin")
+
+		err = os.Chdir(old_dir)
+		LogAndExit(err)
+
+		Copy("/tmp/boot.bin", src+"/boot.bin")
+		Copy(src + "/" + machine + ".dtb",  dest + "/devicetree.dtb")
+		Copy(src + "/core-image-minimal-" + machine + ".cpio.gz.u-boot", dest + "/uramdisk")
+		Copy("resources/uEnv.txt", dest + "/uEnv.txt")
+
+		// Check sizes
+		b.Run("scripts/check_image_files_size.py", dest)
+	case "zc702-zynq7":
+		Copy(src + "/zynq-zc702.dtb", dest)
+		Copy(src + "/boot.scr", dest)
+		Copy(src + "/u-boot.img", dest)
+
+		// Couple of hacks to be able to load the FPGA
+		b.Run("cat " + src + "/uEnv.txt | tr -d '\t' | sed 's/bitstream_image=boot.bin/bitstream_image=fpga.bin/' > " + dest + "/uEnv.txt")
+		b.Run("echo \"bootcmd=run loadfpga && run distro_bootcmd\" >> " + dest + "/uEnv.txt")
+		// cp "$SRC/uEnv.txt" "$DEST" || exit 1
+		Copy(src + "/core-image-minimal-"+machine + ".cpio.gz.u-boot", dest + "/uramdisk.image.gz")
+	case "picozed-zynq7":
+		Copy(src + "/core-image-minimal-"+machine + ".cpio.gz.u-boot", dest + "/uramdisk.image.gz")
+	}
+	
+	// From microzed
+	// cp "$SRC/u-boot.img" "$DEST"
+
+	Copy(src + "/boot.bin", dest)
+	Copy(src + "/uImage", dest)
+
+	// Convert bit to bin. bit format is not compatible
+	b.Run("python resources/fpga-bit-to-bin.py --flip  \"" + bitstream + "\" \"" + dest + "/fpga.bin\"")
 }
